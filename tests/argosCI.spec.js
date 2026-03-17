d41b511d8f9017f7aa40ed20a1d17c472953c994
CZP01 <CZP01@acuitysso.com>
2026-03-17 20:47:30 +0530

    added google

--- tests/argosCI.spec.js
+++ tests/argosCI.spec.js
@@ -2,7 +2,7 @@ import { test } from "@playwright/test";
 import { argosScreenshot } from "@argos-ci/playwright";
 
 test("screenshot homepage", async ({ page }) => {
-  await page.goto("https://facebook.com");
+  await page.goto("https://google.com");
 
   await argosScreenshot(page, "homepage", {
     outputDir: "./argos-output", // ✅ force correct path
