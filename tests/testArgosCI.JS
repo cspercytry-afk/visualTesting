bed2fdb483a97f64f8c71e37c599c3ba374f6188
CZP01 <CZP01@acuitysso.com>
2026-03-17 14:29:59 +0530

    testing google

--- tests/testArgosCI.JS
+++ tests/testArgosCI.JS
@@ -2,6 +2,6 @@ import { test } from "@playwright/test";
 import { argosScreenshot } from "@argos-ci/playwright";
 
 test("screenshot homepage", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+  await page.goto("https://google.com");
   await argosScreenshot(page, "homepage");
 });
