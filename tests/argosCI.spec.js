f05e79e6a7d298de0fbbb47cc44e7044046e84fe
CZP01 <CZP01@acuitysso.com>
2026-03-17 20:52:20 +0530

    added gmail

--- tests/argosCI.spec.js
+++ tests/argosCI.spec.js
@@ -2,7 +2,7 @@ import { test } from "@playwright/test";
 import { argosScreenshot } from "@argos-ci/playwright";
 
 test("screenshot homepage", async ({ page }) => {
-  await page.goto("https://google.com");
+  await page.goto("https://gmail.com");
 
   await argosScreenshot(page, "homepage", {
     outputDir: "./argos-output", // ✅ force correct path
