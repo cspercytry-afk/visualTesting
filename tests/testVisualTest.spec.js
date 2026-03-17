f9aedb4aa9804ad28c6f572cbc1ce1071ee3abb1
CZP01 <CZP01@acuitysso.com>
2026-03-17 12:13:34 +0530

    added google URL

--- tests/testVisualTest.spec.js
+++ tests/testVisualTest.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('example test', async ({ page }) => {
-  await page.goto('https://www.flipkart.com/');
+  await page.goto('https://www.google.com/');
   await expect(page).toHaveScreenshot();
 });
