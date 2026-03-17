3e9ab61bb19a84b9c2b579725eff0400bbe5583e
CZP01 <CZP01@acuitysso.com>
2026-03-17 15:51:06 +0530

    updated code

--- tests/argosCI.spec.js
+++ tests/argosCI.spec.js
@@ -3,5 +3,8 @@ import { argosScreenshot } from "@argos-ci/playwright";
 
 test("screenshot homepage", async ({ page }) => {
   await page.goto("https://google.com");
-  await argosScreenshot(page, "homepage");
+
+  await argosScreenshot(page, "homepage", {
+    outputDir: "./argos-output", // ✅ force correct path
+  });
 });
